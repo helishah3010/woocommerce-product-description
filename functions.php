17a1ccc8a6347a50f13d4db6ded82145adce6e14
Heli <shah_helly3010@yahoo.in>
2019-03-15 14:26:47 -0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -10,3 +10,11 @@ function woocommerce_after_shop_loop_item_title_short_description() {
 	<?php
 }
 add_action('woocommerce_after_shop_loop_item_title', 'woocommerce_after_shop_loop_item_title_short_description', 5);
+		
+/*Hide add to cart/read more button on products page*/
+remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart' );
+
+/*make product click - disabled on product catalog page*/
+remove_action( 'woocommerce_before_shop_loop_item', 'woocommerce_template_loop_product_link_open', 10 );
+remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_product_link_close', 5 );
+
